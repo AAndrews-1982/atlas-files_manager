a06b5ba34ce125ca17b201c55b8e694e95b63a5c
BradenE19 <6115@holbertonstudents.com>
2024-03-12 13:54:15 -0500

    task2 please work already

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
 
 function controllerRouting(app) {
     const router = express.Router();
@@ -13,12 +11,6 @@ function controllerRouting(app) {
   
     router.get('/stats', (req, res) => {
       AppController.getStats(req, res);
-    });
+    })};
 
-    router.get('/connect', (req, res) => {
-        AuthController.getConnect(req, res);
-      });
-    
-      router.get('/disconnect', (req, res) => {
-        AuthController.getDisconnect(req, res);
-      })};
+    export default controllerRouting;
