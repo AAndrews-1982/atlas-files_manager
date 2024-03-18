390440418afc14ba75d9742324fe24eade74d69b
BradenE19 <6115@holbertonstudents.com>
2024-03-18 12:57:34 -0500

    db & redis fix

--- utils/db.js
+++ utils/db.js
@@ -1,42 +1,34 @@
-// utils/db.js
 const { MongoClient } = require('mongodb');
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}`;
 
 class DBClient {
   constructor() {
-    // Environment variables or default values
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-
-    // MongoDB URI and client
-    const uri = `mongodb://${host}:${port}/${database}`;
-    this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-    // Connecting to MongoDB
-    this.client.connect()
-      .then(() => {
-        this.db = this.client.db(database);
-        console.log('Successfully connected to MongoDB');
-      })
-      .catch((err) => console.error(`MongoDB Connection Error: ${err}`));
+    MongoClient.connect(url, (err, client) => {
+      if (!err) {
+        this.db = client.db(database);
+      } else {
+        this.db = false;
+      }
+    });
   }
 
-  // Check if the MongoDB connection is alive
   isAlive() {
-    return !!this.client && !!this.client.topology && this.client.topology.isConnected();
+    if (this.db) return true;
+    return false;
   }
 
-  // Get the number of documents in the users collection
-  nbUsers() {
+  async nbUsers() {
     return this.db.collection('users').countDocuments();
   }
 
-  // Get the number of documents in the files collection
-  nbFiles() {
+  async nbFiles() {
     return this.db.collection('files').countDocuments();
   }
 }
 
-// Create and export an instance of DBClient
 const dbClient = new DBClient();
-module.exports = dbClient;
+export default dbClient;
--- utils/redis.js
+++ utils/redis.js
@@ -1,40 +1,34 @@
-// utils/redis.js
 const redis = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
-    // Initialize the Redis client and handle error events
     this.client = redis.createClient();
-    this.client.on('error', (err) => console.error(`Redis Client Error: ${err}`));
-
-    // Promisify the get, set, and del methods to use async/await
     this.getAsync = promisify(this.client.get).bind(this.client);
-    this.setAsync = promisify(this.client.set).bind(this.client);
-    this.delAsync = promisify(this.client.del).bind(this.client);
+
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error}`);
+    });
   }
 
-  // Check if the Redis connection is alive
   isAlive() {
     return this.client.connected;
   }
 
-  // Get the value for a given key from Redis
   async get(key) {
-    return this.getAsync(key); // Removed redundant await
+    const value = await this.getAsync(key);
+    return value;
   }
 
-  // Set a value in Redis with an expiration time
   async set(key, value, duration) {
-    await this.setAsync(key, value, 'EX', duration);
+    this.client.set(key, value);
+    this.client.expire(key, duration);
   }
 
-  // Delete a key-value pair from Redis
   async del(key) {
-    await this.delAsync(key);
+    this.client.del(key);
   }
 }
 
-// Create and export an instance of RedisClient
 const redisClient = new RedisClient();
-module.exports = redisClient;
+export default redisClient;
