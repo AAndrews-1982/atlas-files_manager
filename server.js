ffd926b9d0ee97f4f144104289d465a426249fc9
BradenE19 <6115@holbertonstudents.com>
2024-03-18 14:32:19 -0500

    task2 fix

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ import router from './routes/index';
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-// Load routes from routes/index.js
 router(app);
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
 });
