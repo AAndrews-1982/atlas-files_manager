c4ffe426208ebbfe82b73243e771277b6e45d745
pretzelogical <yingyangyo266@gmail.com>
2024-03-19 15:42:23 -0500

    try to fix some eslint errors

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,11 +1,34 @@
 import { v4 as uuidv4 } from 'uuid';
 import RedisClient from '../utils/redis';
 import DBClient from '../utils/db';
-import { ServerResponse } from 'http';
 
 const { ObjectId } = require('mongodb');
 const fs = require('fs');
 const Bull = require('bull');
+
+async function getUser(request, response) {
+  const token = request.header('X-Token') || null;
+  if (!token) {
+    response.status(401).send('Unauthorized');
+    return null;
+  }
+
+  const redisToken = await RedisClient.get(`auth_${token}`);
+  if (!redisToken) {
+    response.status(401).send({ error: 'Unauthorized' });
+    return null;
+  }
+
+  const user = await DBClient.db
+    .collection('users')
+    .findOne({ _id: ObjectId(redisToken) });
+  if (!user) {
+    response.status(401).send({ error: 'Unauthorized' });
+    return null;
+  }
+
+  return user;
+}
 
 class FilesController {
   static async postUpload(request, response) {
@@ -15,36 +38,27 @@ class FilesController {
     if (!token) return response.status(401).send({ error: 'Unauthorized' });
 
     const redisToken = await RedisClient.get(`auth_${token}`);
-    if (!redisToken)
-      return response.status(401).send({ error: 'Unauthorized' });
+    if (!redisToken) return response.status(401).send({ error: 'Unauthorized' });
 
-    const user = await DBClient.db
-      .collection('users')
-      .findOne({ _id: ObjectId(redisToken) });
+    const user = await DBClient.db.collection('users').findOne({ _id: ObjectId(redisToken) });
     if (!user) return response.status(401).send({ error: 'Unauthorized' });
 
     const fileName = request.body.name;
     if (!fileName) return response.status(400).send({ error: 'Missing name' });
 
     const fileType = request.body.type;
-    if (!fileType || !['folder', 'file', 'image'].includes(fileType))
-      return response.status(400).send({ error: 'Missing type' });
+    if (!fileType || !['folder', 'file', 'image'].includes(fileType)) return response.status(400).send({ error: 'Missing type' });
 
     const fileData = request.body.data;
-    if (!fileData && ['file', 'image'].includes(fileType))
-      return response.status(400).send({ error: 'Missing data' });
+    if (!fileData && ['file', 'image'].includes(fileType)) return response.status(400).send({ error: 'Missing data' });
 
     const fileIsPublic = request.body.isPublic || false;
     let fileParentId = request.body.parentId || 0;
     fileParentId = fileParentId === '0' ? 0 : fileParentId;
     if (fileParentId !== 0) {
-      const parentFile = await DBClient.db
-        .collection('files')
-        .findOne({ _id: ObjectId(fileParentId) });
-      if (!parentFile)
-        return response.status(400).send({ error: 'Parent not found' });
-      if (!['folder'].includes(parentFile.type))
-        return response.status(400).send({ error: 'Parent is not a folder' });
+      const parentFile = await DBClient.db.collection('files').findOne({ _id: ObjectId(fileParentId) });
+      if (!parentFile) return response.status(400).send({ error: 'Parent not found' });
+      if (!['folder'].includes(parentFile.type)) return response.status(400).send({ error: 'Parent is not a folder' });
     }
 
     const fileDataDb = {
@@ -150,32 +164,8 @@ class FilesController {
       .aggregate(aggregationPipeline)
       .toArray();
 
-    return response.status(200).send(results[0].paginatedResults);
+    response.status(200).send(results[0].paginatedResults);
   }
-}
-
-async function getUser(request, response) {
-  const token = request.header('X-Token') || null;
-  if (!token) {
-    response.status(401).send('Unauthorized');
-    return null;
-  }
-
-  const redisToken = await RedisClient.get(`auth_${token}`);
-  if (!redisToken) {
-    response.status(401).send({ error: 'Unauthorized' });
-    return null;
-  }
-
-  const user = await DBClient.db
-    .collection('users')
-    .findOne({ _id: ObjectId(redisToken) });
-  if (!user) {
-    response.status(401).send({ error: 'Unauthorized' });
-    return null;
-  }
-
-  return user;
 }
 
 module.exports = FilesController;
